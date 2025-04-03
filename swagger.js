9840036666ff004b64a478d093cd55d7bef31d1f
2200032293 <147684853+2200032293@users.noreply.github.com>
2025-04-03 13:29:35 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -9,7 +9,8 @@ const options = {
       version: "1.0.0",
       description: "API documentation for Recipe Finder App",
     },
-    servers: [{ url: "https://recipe-finder-backend-production.up.railway.app/" }],
+    servers: [{ url: "https://recipe-finder-backend-production.up.railway.app/" },
+              {url: "http://34.205.144.252:3000/"}],
   },
   apis: ["./routes/*.js"], // Specify route files for API documentation
 };
@@ -18,7 +19,7 @@ const swaggerSpec = swaggerJsdoc(options);
 
 const swaggerDocs = (app) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger Docs available at https://recipe-finder-backend-production.up.railway.app/api-docs");
+  console.log("Swagger Docs available at https://recipe-finder-backend-production.up.railway.app/api-docs and http://34.205.144.252:3000/api-docs");
 };
 
 module.exports = swaggerDocs;
